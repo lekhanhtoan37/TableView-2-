21d8e335d9e14d43d62c5e30dc3294c7fd8597ba
Toàn Lê <lekhanhtoan37@gmail.com>
2018-11-08 09:46:41 +0700

    test

---
+++ TableView(2)/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableView(2)
+//
+//  Created by Toan on 9/12/18.
+//  Copyright © 2018 Toan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
